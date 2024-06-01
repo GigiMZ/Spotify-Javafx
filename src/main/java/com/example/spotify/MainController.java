8dae9bb2de399eaf58b390e9a2a089febe66559d
GigiMZ <gigitsiklaurimz@gmail.com>
2024-06-01 15:13:21 +0400

    Main page update/design

--- src/main/java/com/example/spotify/MainController.java
+++ src/main/java/com/example/spotify/MainController.java
@@ -1,6 +1,8 @@
 package com.example.spotify;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -8,5 +10,15 @@ import java.util.ResourceBundle;
 public class MainController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {}
+
+    @FXML
+    private ImageView playpause;
+
+    @FXML
+    private ImageView right;
+
+    @FXML
+    private ImageView left;
+
 
 }
