5ce964e4715dd1cec66ea9c114b462eb997c5249
GigiMZ <gigitsiklaurimz@gmail.com>
2024-06-02 22:04:41 +0400

    visual controller set

--- src/main/java/com/example/spotify/MainController.java
+++ src/main/java/com/example/spotify/MainController.java
@@ -2,7 +2,10 @@ package com.example.spotify;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Slider;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,11 +17,65 @@ public class MainController implements Initializable {
     @FXML
     private ImageView playpause;
 
-    @FXML
-    private ImageView right;
+    private static boolean play_pause;
 
     @FXML
-    private ImageView left;
+    private ImageView rightskip;
 
+    @FXML
+    private ImageView leftskip;
+
+    @FXML
+    private ImageView volume;
+
+    @FXML
+    private ImageView avatar;
+
+    @FXML
+    private Slider timeline;
+
+    @FXML
+    private Slider slider_volume;
+
+    @FXML
+    private Text name;
+
+    @FXML
+    private Text authors;
+
+    @FXML
+    private Text current;
+
+    @FXML
+    private Text total;
+
+    @FXML
+    public void play_pause() {
+
+        if (!play_pause) {
+            Image image = new Image("file:///C:/Users/giorg/IdeaProjects/Spotify/src/main/resources/com/example/spotify/pictures/709691.png");
+            playpause.setImage(image);
+            play_pause = true;
+        } else {
+            Image image = new Image("file:///C:/Users/giorg/IdeaProjects/Spotify/src/main/resources/com/example/spotify/pictures/8029490.png");
+            playpause.setImage(image);
+            play_pause = false;
+        }
+    }
+
+    @FXML
+    public void volume_icon() {
+        if (slider_volume.getValue() < 3) {
+
+            Image image = new Image("file:///C:/Users/giorg/IdeaProjects/Spotify/src/main/resources/com/example/spotify/pictures/mute.png");
+            volume.setImage(image);
+        } else if (slider_volume.getValue() < 50) {
+            Image image = new Image("file:///C:/Users/giorg/IdeaProjects/Spotify/src/main/resources/com/example/spotify/pictures/volume-down.png");
+            volume.setImage(image);
+        } else {
+            Image image = new Image("file:///C:/Users/giorg/IdeaProjects/Spotify/src/main/resources/com/example/spotify/pictures/volume-up.png");
+            volume.setImage(image);
+        }
+    }
 
 }
